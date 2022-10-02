951505969787152ceb4f2edd00b98d56399fa3f6
VANMAK <david.vanmak@pop-france.eu>
2022-10-02 12:12:24 +0200

    setUp first Routes

---
+++ promotions-engine/src/Controller/ProductsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductsController extends AbstractController
+{
+    #[Route('/products/{id}/lowest-price', name: 'promotions', methods: 'GET')]
+    public function lowestPrice(int $id): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ProductsController.php',
+        ]);
+    }
+
+    #[Route('/products/{id}/promotions', name: 'promotions', methods: 'GET')]
+    public function promotions(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ProductsController.php',
+        ]);
+    }
+}
