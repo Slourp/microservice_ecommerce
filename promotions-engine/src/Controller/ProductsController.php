2b79fa3b904ec3f09d82593f790f0234b92c639a
VANMAK <david.vanmak@pop-france.eu>
2022-10-13 16:04:40 +0200

    Revert "Revert "[ADD] add a custom Serializer""
    This reverts commit e17155b3f0e154d18dd54908244615428bd46a9b.

--- promotions-engine/src/Controller/ProductsController.php
+++ promotions-engine/src/Controller/ProductsController.php
@@ -2,18 +2,19 @@
 
 namespace App\Controller;
 
-use App\DTO\LowestPriceEnquiry;
+use App\DTO\LowestPriceEnquery\LowestPriceEnquiry;
+use App\Service\Serializer\DTOSerializer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
+// use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductsController extends AbstractController
 {
-    #[Route('/products/{id}/lowest-price', name: 'products.lowestPrice', methods: 'GET')]
-    public function lowestPrice(Request $request, int $id, SerializerInterface $serializer): Response
+    #[Route('/products/{id}/lowest-price', name: 'products.lowestPrice', methods: 'POST')]
+    public function lowestPrice(Request $request, int $id, DTOSerializer $serializer): Response
     {
         if ($request->headers->has('Force-Fail')) {
             $ERROR_CODE = $request->headers->get('Force-Fail');
@@ -23,23 +24,25 @@ class ProductsController extends AbstractController
 
             );
         }
-        // dd($serializer);
+
+        /**
+         * @var LowestPriceEnquiry $lowestPriceEnquiry
+         */
         $lowestPriceEnquiry = $serializer->deserialize(
             $request->getContent(),
             LowestPriceEnquiry::class,
             'json'
         );
-        dd($lowestPriceEnquiry);
 
-        return $this->json([
-            'quantity' => 5,
-            'request_location' => "UK",
-            'voucher_code' => 'OU812',
-            'request_date' => '2022-04-04',
-            'product_id' => $id,
-            'discounted_price' => 50,
-            'promotion_id' => 3,
-            'promotion_name' => 'Black Friday half price sale',
-        ]);
+
+        $lowestPriceEnquiry
+            ->setDiscountedPrice(50)
+            ->setPrice(50)
+            ->setPromotionId(3)
+            ->setPromotionName('Black Friday half price sale');
+
+        $response = $serializer->serialize($lowestPriceEnquiry, 'json');
+
+        return new Response($response, 200);
     }
 }
